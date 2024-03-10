48dee57f1919df98c01eb13b581c0015fad76389
Aditya Singh <sengaraditya01@gmail.com>
2024-03-10 09:04:34 +0530

    Improved

--- src/components/github/Github.jsx
+++ src/components/github/Github.jsx
@@ -2,29 +2,40 @@ import React from "react";
 import { useState } from "react";
 import { useLayoutEffect } from "react";
 import { useEffect } from "react";
-import {useLoaderData} from "react-router-dom"
+import { useLoaderData, Link } from "react-router-dom";
 function Github() {
-    const data = useLoaderData();
-//   const [data, setData] = useState([]);
-//   useEffect(() => {
-//     fetch("https://api.github.com/users/Thakur-Aditya")
-//       .then((response) => response.json())
-//       .then((data) => {
-//         console.log(data);
-//         setData(data);
-//       });
-//   }, []);
+  const data = useLoaderData();
+  //   const [data, setData] = useState([]);
+  //   useEffect(() => {
+  //     fetch("https://api.github.com/users/Thakur-Aditya")
+  //       .then((response) => response.json())
+  //       .then((data) => {
+  //         console.log(data);
+  //         setData(data);
+  //       });
+  //   }, []);
   return (
     <div>
-      Github followers :{data.followers}
-      <img src={data.avatar_url} alt="" />
+      Github followers :{data.followers} <br />
+      <b>Link: </b>
+      <Link
+        to="https://github.com/Thakur-Aditya/Understanding-react-router.git"
+        className="text-red-500"
+      >
+        GitHub link
+      </Link>
+      <img
+        src={data.avatar_url}
+        className="h-24 rounded-full"
+        alt="Github avatar"
+      />
     </div>
   );
 }
 
 export default Github;
 
-export const gitHubLoader = async ()=>{
-    const response = await fetch("https://api.github.com/users/Thakur-Aditya")
-    return response.json();
-}
+export const gitHubLoader = async () => {
+  const response = await fetch("https://api.github.com/users/Thakur-Aditya");
+  return response.json();
+};
